9ee006312ff8db4f93bdf3ba0ea4d9beeceab6d1
Kaisensage <kvngbabstar@gmail.com>
2025-06-20 08:12:46 +0100

    June 20

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -74,7 +74,7 @@ export const About = () => {
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong> University Of Lagos </strong> 
+                  <strong> University Of Lagos (2021 - 2025){" "}</strong> 
                 </li>
                 
                 <li>
